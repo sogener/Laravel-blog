07407f3a9674e1938c113f79a432e16b7d6337c4
Gleb Kalinin <g.kalinin@iqdev.digital>
2021-08-31 09:14:00 +0500

    Layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,22 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}"></script>
-</head>
-<body>
-<div class="container mt-4">
+@extends('layouts.app')
+
+@section('header')
     <h1>О блоге</h1>
+@endsection
+
+@section('content')
     <p>Эксперименты с Laravel</p>
-    <div class="team" style="text-align: center">
+@endsection
+
+<div class="team">
+    <h1 class="team__title">Our team!</h1>
+    <div class="team__inside">
+        @foreach($ourTeam as $member)
+            <h2><?=$member['name']?></h2>
+            <p><?=$member['position']?></p>
+        @endforeach
+    </div>
+    <div class="team">
         <h1 class="team__title">Our team!</h1>
         <div class="team__inside">
             @foreach($ourTeam as $member)
@@ -21,5 +26,3 @@
         </div>
     </div>
 </div>
-</body>
-</html>
